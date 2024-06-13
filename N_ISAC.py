d5feab1d7c23b59cf78ffaa006d3f4b4c2245269
SpikingJ <109554807+SpikingJ@users.noreply.github.com>
2024-06-13 14:56:26 +0100

    N_ISAC.py

--- N_ISAC.py
+++ N_ISAC.py
@@ -1,7 +1,7 @@
 
-# The N-ISAC paper is implemented using 'snn' package in https://github.com/kclip/snn, which has not been updated for a while.
+# The N-ISAC paper is implemented using 'snn' package in https://github.com/kclip/snn, which has not been updated for some time and is somewhat challenging to use.
 # This implementation is based on the 'snntorch' https://snntorch.readthedocs.io/en/latest/, which is easier to implement and see how N-ISAC works.
-# It may also yield better results and could serve as a starting point for further extensions. 
+# It offers various hyperparameters to experiment with, which may yield better results and serve as a starting point for further extensions.
 
 import numpy as np
 import torch
